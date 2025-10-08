27f71dfada876dee9c59846962e4f7a09ba78704
EnzoAmarilla <enzo100amarilla@gmail.com>
2025-10-08 15:14:46 -0300

    simular usuario admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::firstOrCreate(
+            ['email' => 'admin@perfilesprofesionales.com'],
+            [
+                'name' => 'Administrador',
+                'password' => Hash::make('123456'),
+                'phone' => '1122334455',
+                'profile_user_id' => 1, // ⚠️ Ajustá según exista el perfil admin
+                'locality_id' => 1, // ⚠️ Ajustá según exista el perfil admin
+            ]
+        );
+
         $this->call([
             CountriesSeeder::class,
             ProvincesSeeder::class,
