6178253f138b009097b7c58452c0fe164cb1ceb1
EnzoAmarilla <enzo100amarilla@gmail.com>
2025-10-09 13:14:08 -0300

    avances ABM actividades

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -3,11 +3,38 @@
 namespace App\Http\Controllers;
 
 use App\Models\Activity;
+use Illuminate\Http\Request;
 
 class ActivityController extends Controller
 {
     public function index()
     {
         return response()->json(["data" => Activity::all()]);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|unique:activities,name',
+        ]);
+
+        $activity = Activity::create($request->all());
+        return response()->json(["data" => $activity], 201);
+    }
+
+    public function update(Request $request, Activity $activity)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|unique:activities,name,' . $activity->id,
+        ]);
+
+        $activity->update($request->all());
+        return response()->json(["data" => $activity]);
+    }
+
+    public function destroy(Activity $activity)
+    {
+        $activity->delete();
+        return response()->json(["message" => "Actividad eliminada con exito"]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\CountryController;
 use App\Http\Controllers\LocalityController;
 use App\Http\Controllers\ProfileUserController;
 
-Route::get('/activities', [ActivityController::class, 'index']);
+// Route::get('/activities', [ActivityController::class, 'index']);
 Route::get('/countries', [CountryController::class, 'index']);
 Route::get('/localities', [LocalityController::class, 'index']);
 Route::get('/provinces/{province}/localities', [LocalityController::class, 'byProvince']);
@@ -22,5 +22,6 @@ Route::middleware('auth:api')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
 
     // ABM usuarios y perfiles (protegido)
+    Route::resource('activities', ActivityController::class);
     Route::resource('users', UserController::class);
 });
